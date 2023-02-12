773cfe521baddfa3197359406aa2db9d51029bb2
omar saadeh <omarsaadeh11@gmail.com>
2023-02-12 03:46:12 -0800

    designing app

--- omarwork/src/components/Navigation/MainNavigation.js
+++ omarwork/src/components/Navigation/MainNavigation.js
@@ -1,31 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import Backdrop from "../UIElements/Backdrop";
+import SideDrawer from "./SideDrawer";
 import classes from "./MainNavigation.module.css";
 import NavLinks from "./NavLinks";
 
 const MainNavigation = () => {
-  return (
-    <header className={classes.header}>
-      <div className={classes.mainNavigation}>
-        <h1 className={classes.mainNavigationTitle}>
-          <span className={classes.mainNavigationLeft}>
-            <img
-              className={classes.mainNavigationImg}
-              src="https://www.howiework.com/static/media/logo.73fe7a7a.png"
-              alt="logo"
-            />
-            <a className={classes.mainNavigationImgTitle}>Omar Work</a>
-          </span>
-        </h1>
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-        <div className={classes.mainNavigationList}>
-          <img />
-          <nav>
-            <NavLinks />
-          </nav>
-          <button></button>
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+  return (
+    <>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      {/* Mobile */}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className={`${classes["main-navigation__drawer-nav"]}`}>
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
+      <header className={classes.header}>
+        <div className={classes.mainNavigation}>
+          <h1 className={classes.mainNavigationTitle}>
+            <span className={classes.mainNavigationLeft}>
+              <img
+                className={classes.mainNavigationImg}
+                src="https://www.howiework.com/static/media/logo.73fe7a7a.png"
+                alt="logo"
+              />
+              <a className={classes.mainNavigationImgTitle}>Omar Work</a>
+            </span>
+          </h1>
+
+          <div className={classes.mainNavigationList}>
+            <img />
+            <nav className={`${classes["main-navigation__header-nav"]}`}>
+              <NavLinks />
+            </nav>
+            <button
+              className={`${classes["main-navigation__menu-btn"]}`}
+              onClick={openDrawerHandler}
+            >
+              <span />
+              <span />
+              <span />
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
